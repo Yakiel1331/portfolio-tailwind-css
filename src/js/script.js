98e66323100074e41b242eb9a7612691f92c283b
yakiel1331 <audryanhn1331@gmail.com>
2024-09-07 01:12:10 +0700

    Update Dark Mode and Fixes some features

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,68 @@
 //Navbar Fixed
 
-window.onscroll = function(){
+function handlescroll(){
     const header = document.querySelector('header');
     const Fixednav = header.offsetTop;
+    const ToTop = document.querySelector('#to-top');
 
-    if( window.pageYOffset > Fixednav){
+    if( window.scrollY > Fixednav){
         header.classList.add('navbar-fixed');
+        ToTop.classList.remove('hidden');
+        ToTop.classList.add('flex');
     }else{
         header.classList.remove('navbar-fixed');
+        ToTop.classList.remove('flex');
+        ToTop.classList.add('hidden');
+        
     };
 
 }
+
+
+window.onscroll = handlescroll; 
+window.onload = handlescroll;
+
+
 
 
 // Hamburger
 
 const NavMenu = document.querySelector('#nav-menu');
 const hamburger = document.querySelector('#hamburger');
+const DarkModeToggle = document.querySelector('#dark-toggle')
 
 hamburger.addEventListener('click',function(){
     hamburger.classList.toggle('hamburger-active');
     NavMenu.classList.toggle('hidden');
 })
+
+//Klik dilluar Hamburger
+
+window.addEventListener('click', function(e){
+    if(e.target != hamburger && e.target != NavMenu && e.target != DarkModeToggle){
+        hamburger.classList.remove('hamburger-active');
+        NavMenu.classList.add('hidden');
+    };
+});
+
+//Darkmode toggle
+
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+    if(html.classList.contains('dark')){
+        darkToggle.checked = true;
+    }else{
+        darkToggle.checked = false;
+    }
+
+darkToggle.addEventListener('click', function(){
+
+    if(darkToggle.checked){
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    }else{
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html'],
+  darkMode : 'class',
   theme: {
     container: {
       center: true,
@@ -12,7 +13,8 @@ module.exports = {
       colors: {
         primary : '#14b8a6',
         dark : '#1e293b',
-        secondary : '#64748b'
+        secondary : '#64748b',
+        darksecondary : '#94a3b8',
       },
       screens: {
         '2xl': '1140px',
